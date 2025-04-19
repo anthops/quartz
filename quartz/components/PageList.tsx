014eabed526fe0d9751db1bf1b02af4738dcef4e
Tony <32993852+anthops@users.noreply.github.com>
2025-04-20 00:24:51 +0930

    feat: configure tag page to link to overridden urls #1915

--- quartz/components/PageList.tsx
+++ quartz/components/PageList.tsx
@@ -78,7 +78,7 @@ export const PageList: QuartzComponent = ({ cfg, fileData, allFiles, limit, sort
               </p>
               <div class="desc">
                 <h3>
-                  <a href={resolveRelative(fileData.slug!, page.slug!)} class="internal">
+                  <a href={resolveRelative(fileData.slug!, page.urlOverride ?? page.slug!)} class="internal">
                     {title}
                   </a>
                 </h3>
